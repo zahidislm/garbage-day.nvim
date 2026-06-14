c69b4df2d9c448e6cf0036db126fb4800020f80a
Zahid Islam <4127821+zahidislm@users.noreply.github.com>
2026-06-14 02:44:59 -0400

    Refactor LSP client management with timer integration

--- lua/garbage-day/utils.lua
+++ lua/garbage-day/utils.lua
@@ -1,17 +1,18 @@
 local M = {}
 
+local config = vim.g.garbage_day_config
+local start_timer = vim.uv.new_timer()
+
 -- CORE UTILS
 -- ----------------------------------------------------------------------------
 
 ---Stop all LSP clients, including the ones in other tabs.
 function M.stop_lsp()
-  local config = vim.g.garbage_day_config
   for _, client in pairs(vim.lsp.get_clients()) do
-    local is_lsp_client_excluded =
-      vim.tbl_contains(config.excluded_lsp_clients, client.name)
+    local is_lsp_client_excluded = vim.tbl_contains(config.excluded_lsp_clients, client.name)
 
-    -- Stop lsp client
     if not is_lsp_client_excluded then
+      stopped_clients_cache[client.name] = true
       client:stop(true)
     end
   end
@@ -20,35 +21,28 @@ end
 ---Start LSP clients for the current buffer.
 ---It will retry for a configurable amount of times.
 function M.start_lsp()
-  local config = vim.g.garbage_day_config
-  local total_retries = config.retries
-  local duration = config.timeout
-
-  local timer = vim.uv.new_timer()
   local elapsed_retries = 0
+
+  if start_timer:is_active() then
+    start_timer:stop()
+  end
 
   local timer_callback
   timer_callback = vim.schedule_wrap(function()
-    -- Check if the desired number of retries has been met
-    if elapsed_retries >= total_retries then
-      timer:stop()
-      timer:close()
+    if elapsed_retries >= config.retries then
+      start_timer:stop()
       return
     end
 
-    -- Start LSP
-    for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
-      vim.lsp.enable(client.name)
+    -- Re-enable the specific clients that were halted
+    for client_name, _ in pairs(stopped_clients_cache) do
+      vim.lsp.enable(client_name)
     end
 
     elapsed_retries = elapsed_retries + 1
-
-    -- Schedule the next trigger
-    timer:start(duration, 0, timer_callback)
   end)
 
-  -- Start the timer for the first trigger
-  timer:start(duration, 0, timer_callback)
+  start_timer:start(config.timeout, config.timeout, timer_callback)
 end
 
 -- MISC UTILS
