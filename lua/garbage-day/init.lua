c9166cdc1257c8dd711ecef81c578c635859c3c8
Zahid Islam <dev@zahidislm.com>
2026-07-12 02:28:38 -0400

    refactor: Make `setup()` just a bridge for `lazy,nvim`
    - No need to run `setup()`, plugin is lazy loaded
    - uses global space `vim.g.GarbageDay` to store `config`

--- lua/garbage-day/init.lua
+++ lua/garbage-day/init.lua
@@ -22,17 +22,16 @@ local grace_timer = assert(vim.uv.new_timer())
 local debounce_timer = assert(vim.uv.new_timer())
 
 local state = { stopped = false, waking_up = false }
+local c = vim.g.GarbageDay.config
 
----@return GarbageDay.Config
-local function cfg()
-  return vim.g.garbage_day_config
-end
+---@type string
+local current_filetype
 
 function M.on_focus_lost()
   state.waking_up = false
 
   grace_timer:start(
-    cfg().grace_period * 1000, 0,
+    c.grace_period * 1000, 0,
     vim.schedule_wrap(function ()
       if not state.stopped then
         lsp.stop()
@@ -57,29 +56,27 @@ function M.on_focus_gained()
     end
 
     state.stopped = false
-  end, cfg().wakeup_delay)
+  end, c.wakeup_delay)
 end
 
----@type string
-local current_filetype
-
 function M.on_buf_enter(args)
-  local c = cfg()
   if not c.aggressive_mode then
     return
   end
 
   local ft = vim.bo[args.buf].filetype
   local buftype = vim.bo[args.buf].buftype
+  local changed = current_filetype and ft ~= current_filetype
+  current_filetype = ft
+
   if vim.tbl_contains(c.aggressive_mode_ignore.filetype, ft) then
     return
   end
+
   if vim.tbl_contains(c.aggressive_mode_ignore.buftype, buftype) then
     return
   end
 
-  local changed = ft ~= current_filetype
-  current_filetype = ft
   if not changed then
     return
   end
@@ -106,8 +103,7 @@ end
 
 ---@param opts? table
 function M.setup(opts)
-  require("garbage-day.config").set(opts)
-  current_filetype = vim.bo.filetype
+  vim.g.GarbageDay.config = vim.tbl_extend("force", c, opts or {})
 end
 
 return M
